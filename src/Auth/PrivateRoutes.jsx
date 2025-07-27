4cbc3c2147937070cd040a7c4e41a49d8801f2a0
A-Farabi <farabisyl2017@gmail.com>
2025-07-27 19:16:22 +0600

    loader added on privatesRoutes

--- src/Auth/PrivateRoutes.jsx
+++ src/Auth/PrivateRoutes.jsx
@@ -1,21 +1,25 @@
 import { useContext } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
+import { NewtonsCradle } from "ldrs/react";
+import "ldrs/react/NewtonsCradle.css";
 
-const PrivateRoutes = ({children}) => {
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext)
-
-    if (user) {
-        return children
-    }
-    
-    
+  if (loading) {
+    // Default values shown
     return (
-        <div>
-           <Navigate to="/login"></Navigate>
+      <div className="flex justify-center items-center center h-screen">
+        <div className="flex justify-center flex-col items-center">
+            <NewtonsCradle size="100" speed="1.4" color="red" />
+        <h1 className="text-xl mt-3">Component is loading, Please wait...</h1>
         </div>
+              </div>
     );
+  }
+
+  return user ? children : <Navigate to="/login"></Navigate>;
 };
 
 export default PrivateRoutes;
